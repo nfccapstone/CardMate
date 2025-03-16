1062d5dae90d233565bd30afb34ba578d8c1060b
hwang <dororojs@gmail.com>
2025-03-16 09:44:11 +0900

    Test2

--- backend_flask/app.py
+++ backend_flask/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Flask on EC2! new code new flask!"
+    return "Hello, Flask on EC2! new code new flask new ddddflaks flnew !"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
