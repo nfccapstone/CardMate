5da471a9d8d8491d7156d90ef4ab71a7bed5765a
hblue <dororojs@gmail.com>
2025-03-20 12:52:51 +0900

    CICD5

--- backend_flask/app.py
+++ backend_flask/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Flask oddddddnsddddss EC2! docker! !"
+    return "Hello, Flask oddddddnsddsssddss EC2! docker! !"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
