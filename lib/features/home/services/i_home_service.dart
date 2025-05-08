43fd743720fefcf4678a4088bc16dbc87ab6ccf6
Minki Cho <minki0104@naver.com>
2025-05-08 13:20:24 +0900

    데이터베이스 구조 설정

--- lib/features/home/services/i_home_service.dart
+++ lib/features/home/services/i_home_service.dart
@@ -4,7 +4,7 @@ abstract class IHomeService {
   /// 현재 로그인한 사용자의 기본 명함 정보 불러오기 (users/{uid})
   Future<Map<String, dynamic>?> fetchCardData();
   
-  /// 연락처 정보 불러오기 (users/{uid})
+  /// 연락처 정보 불러오기 (users/{uid}/card_contact/contacts)
   Future<Map<String, String>?> fetchContactInfo();
   
   /// 기본 명함 정보 수정 (users/{uid})
@@ -12,4 +12,10 @@ abstract class IHomeService {
   
   /// 명함 등록 여부 확인 (기본 정보 문서 존재 여부)
   Future<bool> checkCardExists();
+
+  /// 명함 블록 정보 불러오기 (users/{uid}/card_block)
+  Future<List<Map<String, dynamic>>> fetchCardBlocks();
+
+  /// 명함 스타일 정보 불러오기 (users/{uid}/card_style/style)
+  Future<Map<String, dynamic>?> fetchCardStyle();
 }
